7fbb58f46c33be30b8e02ff9f9be918bfffebb3b
sumit jain <sumitjain8061@gmail.com>
2025-03-11 16:57:34 +0530

    update homebanner

--- client/src/components/HomeBanner.jsx
+++ client/src/components/HomeBanner.jsx
@@ -1,29 +1,51 @@
-import React from 'react'
-import Lottie from 'lottie-react' // Assuming you're using lottie-react for animation
-// import Herobg from '../assets/herobg.jpg'
+import React from "react";
+// import Lottie from "lottie-react"; // Assuming you're using lottie-react for animation
+import Herobg from '../assets/homeupdated8.jpg'
+import newgif from "../assets/homegif8.webp"
 
 const HomeBanner = ({ text, animationData }) => {
   return (
-    <div 
-      className="w-full h-screen relative flex flex-col sm:flex-row justify-center items-center py-8 md:pl-10"
-      // style={{ backgroundImage: `url(${Herobg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    <div
+      className="w-full h-[35rem] relative flex flex-col sm:flex-row justify-center items-center py-8 md:pl-10 "
+      style={{ backgroundImage: `url(${Herobg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
       {/* Background Image */}
-      <div className="absolute inset-0 bg-black opacity-60 sm:block hidden"></div> 
+      <div className="absolute inset-0  sm:block hidden"></div>
 
       {/* Left side - Text */}
-      <div className="relative text-white text-center sm:text-left sm:w-1/2 px-10 sm:px-8 z-10">
-        <h1 className="text-4xl sm:text-5xl font-semibold mb-4">{text.title}</h1>
-        <p className="text-lg sm:text-xl mb-6">{text.description}</p>
+      <div className="relative text-white text-left sm:text-left sm:w-1/2 px-10 sm:px-8  ">
+        <h1 className="md:text-8xl sm:text-5xl font-semibold font-serif mb-4">
+          {text.title}
+        </h1>
+        <br />
+        <br />
+
+        <p className="md:text-3xl uppercase sm:text-xl mb-6  font-semibold">
+          {text.type1}
+          <br />
+          <br />
+          {text.type2}
+          <br />
+          <br />
+          {text.type3}
+          <br />
+          <br />
+          {text.type4}
+        </p>
         {/* You can add additional elements here, like buttons, links, etc. */}
       </div>
 
       {/* Right side - Lottie Animation */}
-      <div className="relative sm:w-1/2 flex justify-center items-center mt-0 md:mt-8 sm:mt-0 z-10">
-        <Lottie animationData={animationData} loop={true} className="w-full max-w-xl h-auto" />
+      <div className="relative h-full w-auto  ">
+        {/* <Lottie
+          animationData={animationData}
+          loop={true}
+          className="w-full max-w-xl h-auto"
+        /> */}
+        <img src={newgif} className="rounded-2xl    "/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
