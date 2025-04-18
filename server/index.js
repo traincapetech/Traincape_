528dad0ea0cb6be2b651e203c40602aa455acd04
Ishaan Jain <ishaanj2612@gmail.com>
2025-04-18 15:21:04 +0530

    pulled main code

--- server/index.js
+++ server/index.js
@@ -120,8 +120,14 @@ dotenv.config();
 
 const app = express();
 
+const corsOptions = {
+  origin: 'https://traincapetech.in',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 app.use("/questions", questionRouter);  
 app.use("/results", resultRouter);
